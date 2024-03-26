74cd8a4e6a4d7a39c0e22f61f1fbf81f68d2d697
Tony Nguyen <tonynguyen7396@gmail.com>
2024-03-26 13:43:42 +0100

    Defined VendingMachine interface

--- src/main/java/se/lexicon/group2/model/VendingMachine.java
+++ src/main/java/se/lexicon/group2/model/VendingMachine.java
@@ -1,5 +1,10 @@
 package se.lexicon.group2.model;
 
 public interface VendingMachine {
-
+void addCurrency(int amount);
+int getBalance();
+Product request(Product id);
+int endSession();
+String getDescription(Product id);
+String[] getProducts();
 }
---
+++ src/main/java/se/lexicon/group2/model/VendingMachineImplement.java
@@ -0,0 +1,5 @@
+package se.lexicon.group2.model;
+
+public class VendingMachineImplement {
+
+}
